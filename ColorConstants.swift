2b85bef022e0453f6aa40d1c185738738f068914
VuTranQuang <vutran7993@gmail.com>
2019-11-07 14:02:50 +0700

    Một số màu

---
+++ ColorConstants.swift
@@ -0,0 +1,142 @@
+
+import Foundation
+
+
+
+final class ColorConstants: NSObject {
+    
+  
+    static let twnYellowColor = #colorLiteral(red: 0.9882352941, green: 0.7843137255, blue: 0, alpha: 1)
+    
+
+    static let whiteColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    
+  
+    static let grayColor = #colorLiteral(red: 0.7019607843, green: 0.7019607843, blue: 0.7019607843, alpha: 1)
+    
+   
+    static let grayColorFrame = #colorLiteral(red: 0.8901960784, green: 0.8901960784, blue: 0.8901960784, alpha: 1)
+    
+
+    static let grayInactiveColorFrame = #colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9019607843, alpha: 1)
+    
+ 
+    static let lightGrayColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+    
+
+    static let lightGrayColor2 = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
+    
+  
+    static let lightGrayColor3 = #colorLiteral(red: 0.9176470588, green: 0.9176470588, blue: 0.9098039216, alpha: 1)
+    
+
+    static let backgroundColor = #colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9137254902, alpha: 1)
+    
+   
+    static let separatorColor = #colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)
+    
+
+    static let titleTextColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+
+    static let keepColor = #colorLiteral(red: 0.9882352941, green: 0.6078431373, blue: 0, alpha: 1)
+    
+
+    static let linkColor = #colorLiteral(red: 0.1568627451, green: 0.5294117647, blue: 0.9215686275, alpha: 1)
+    
+
+    static let cellDetailColor = #colorLiteral(red: 0.262745098, green: 0.2509803922, blue: 0.2156862745, alpha: 1)
+    
+
+    static let cellTitleColor = #colorLiteral(red: 0.368627451, green: 0.368627451, blue: 0.368627451, alpha: 1)
+    
+
+    static let lineBorderColor = #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+    
+
+    static let updateInfoLineBorderColor = #colorLiteral(red: 0.7960784314, green: 0.7960784314, blue: 0.7960784314, alpha: 1)
+    
+
+    static let boldBlackColor = #colorLiteral(red: 0.1019607843, green: 0.1019607843, blue: 0.1019607843, alpha: 1)
+    
+
+    static let countdownRedColor = #colorLiteral(red: 1, green: 0.3333333333, blue: 0.3137254902, alpha: 1)
+    
+
+    static let optionalGrayColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+    
+
+    static let blackColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    
+
+    static let redColor = #colorLiteral(red: 1, green: 0.3333333333, blue: 0.3137254902, alpha: 1)
+    
+
+    static let peachColor = #colorLiteral(red: 0.9843137255, green: 0.1176470588, blue: 0.4705882353, alpha: 1)
+    
+
+    static let darkRedColor = #colorLiteral(red: 0.9411764706, green: 0.3137254902, blue: 0.2941176471, alpha: 1)
+    
+
+    static let genderMaleColor = #colorLiteral(red: 0.2745098039, green: 0.7058823529, blue: 0.9411764706, alpha: 1)
+    
+
+    static let genderFemaleColor = #colorLiteral(red: 0.9803921569, green: 0.3921568627, blue: 0.5098039216, alpha: 1)
+    
+
+    static let blueButtonHighlightedColor = #colorLiteral(red: 0.1568627451, green: 0.5294117647, blue: 0.9215686275, alpha: 1)
+    
+
+    static let defaultBlueColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+    
+
+    static let darkGrayColor = #colorLiteral(red: 0.5019607843, green: 0.5019607843, blue: 0.5019607843, alpha: 1)
+    
+
+    static let darkGrayColor2 = #colorLiteral(red: 0.4509803922, green: 0.4509803922, blue: 0.4509803922, alpha: 1)
+    
+
+    static let darkGrayColor3 = #colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+    
+
+    static let bannerColorRed = #colorLiteral(red: 1, green: 0.4274509804, blue: 0.4235294118, alpha: 1)
+    
+
+    static let bannerColorBlue = #colorLiteral(red: 0.2235294118, green: 0.737254902, blue: 0.737254902, alpha: 1)
+    
+
+    static let bannerColorGreen = #colorLiteral(red: 0.368627451, green: 0.7411764706, blue: 0.2823529412, alpha: 1)
+    
+
+    static let noSearchConditionGrayColor = #colorLiteral(red: 0.7960784314, green: 0.7960784314, blue: 0.7960784314, alpha: 1)
+    
+
+    static let topTappedGrayColor = #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)
+    
+
+    static let buttonMessageDisableGrayColor = #colorLiteral(red: 0.7843137255, green: 0.7843137255, blue: 0.7843137255, alpha: 1)
+    
+
+    static let buttonBorderDisableGrayColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.9490196078, alpha: 1)
+
+
+    static let jolistImageLoadingColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+    
+
+    static let cassetteExpiredMaskColor = #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9568627451, alpha: 1)
+    
+    
+
+    static let chatErrorViewOffLineColor = #colorLiteral(red: 0.5019607843, green: 0.5019607843, blue: 0.5019607843, alpha: 1)
+    
+    static let chatSendButtonEnableColor = #colorLiteral(red: 1, green: 0.3333333333, blue: 0.3137254902, alpha: 1)
+    
+    static let chatSendButtonDisableColor = #colorLiteral(red: 0.8392156863, green: 0.8392156863, blue: 0.8392156863, alpha: 1)
+    
+    static let appliedChatButtonBackgroundColor = #colorLiteral(red: 0.1568627451, green: 0.5254901961, blue: 0.9176470588, alpha: 1)
+    
+    
+    #if DEBUG
+    static let debugDeveloperTitleColor = #colorLiteral(red: 1, green: 0, blue: 0.3921568627, alpha: 1)
+    #endif
+}
+
